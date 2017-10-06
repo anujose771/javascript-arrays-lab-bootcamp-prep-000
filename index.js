4055ef510da14c72751ad0c9c29634b94556853e
blue shadow <anujose1989@gmail.com>
2017-10-06 04:44:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 const app = "I don't do much."
+var kittens = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendKitten(name){
+  return kittens.push(name);
+}
+destructivelyAppendKitten('Ralph');
+
+function destructivelyPrependKitten(name){
+  return kittens.unshift(name);
+}
+destructivelyPrependKitten("Bob")
+
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+  return kittens;
+}
+destructivelyRemoveLastKitten();
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+  return kittens;
+}
+destructivelyRemoveFirstKitten();
+
+
+function appendKitten(name){
+  return [...kittens, name];
+}
+appendKitten("Broom");
+
+function prependKitten(name){
+  return [name, ...kittens ];
+}
+prependKitten("Arnold");
+
+function removeLastKitten(name){
+  return slice(-1);
+}
+removeLastKitten();
+
+// function removeFirstKitten(name){
+//   return slice(1);
+// }
+// removeFirstKitten();
